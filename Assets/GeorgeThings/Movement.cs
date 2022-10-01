0d3a2df266a4c5c5d8b2d0854af8fc71538cb43a
georgeshea <52836004+GeorgeJShea@users.noreply.github.com>
2022-10-01 14:01:38 -0500

    work in progrees movment and flair

---
+++ Assets/GeorgeThings/Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    private Vector2 moveDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Inputs();
+    }
+
+    private void Inputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY);
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDir.x * moveSpeed * Time.deltaTime, moveDir.y * moveSpeed * Time.deltaTime);
+    }
+}

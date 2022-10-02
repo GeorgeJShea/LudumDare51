9e0b9f4d9b340466336690de2aa9f2c166869aca
SomeStupidSongBird <563ftw@gmail.com>
2022-10-02 18:48:30 -0500

    minor change

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,7 +7,8 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private int damage;
     [SerializeField] private float speed;
-    [SerializeField] public float active_time = 4;
+    [SerializeField] public int max_active_time = 4;
+    private float active_time = 0;
     [SerializeField] private Rigidbody2D bullet;
 
     void Awake()
@@ -27,13 +28,13 @@ public class Bullet : MonoBehaviour
     // any extra bullet movement things will go in here
     void Update()
     {
-        if(active_time<=0)
+        if(active_time>=max_active_time)
         {
             Debug.Log("Bullet timed out");
-            active_time = 4;
+            active_time = 0;
             MasterObjectPooler.Instance.Release(this.gameObject, "Bullet");
         }
-        active_time-=Time.deltaTime;
+        active_time+=Time.deltaTime;
     }
     private void OnCollisionEnter2D(Collision2D col)
     {

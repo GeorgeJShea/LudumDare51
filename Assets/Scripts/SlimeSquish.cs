f51067ac587c627c632481662467b371d9e4ca50
SomeStupidSongBird <563ftw@gmail.com>
2022-10-03 15:23:53 -0500

    slimeSquish is now more dynamic

--- Assets/Scripts/SlimeSquish.cs
+++ Assets/Scripts/SlimeSquish.cs
@@ -6,19 +6,19 @@ public class SlimeSquish : MonoBehaviour
 {
 
     public float speed = 2;
-    public float offset = 2;
     public float pulse = 1;
+    public Vector3 defaultSize;
     // Start is called before the first frame update
     void Start()
     {
-        
+        defaultSize = gameObject.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Vector3 vec = new Vector3(Mathf.Sin(pulse * Time.time * speed) + offset, pulse * Mathf.Sin(Time.time * speed) + offset, pulse * Mathf.Sin(Time.time * speed) + offset);
-        Vector3 vec = new Vector3(2, pulse * Mathf.Sin(Time.time * speed) + offset, 2);
+        Vector3 vec = new Vector3(defaultSize.x, pulse * Mathf.Sin(Time.time * speed) + defaultSize.y, defaultSize.z);
         transform.localScale = vec;
     }
 }
